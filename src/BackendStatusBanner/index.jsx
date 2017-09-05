7dc98c88b89b921bfde93ac549aff00d04baa9f2
Ari Rizzitano <arizzitano@edx.org>
2017-09-05 14:30:27 -0400

    redux working

--- src/BackendStatusBanner/index.jsx
+++ src/BackendStatusBanner/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import statusMap from './statusMap.json';
+import { pingStudio } from '../data/store';
 
 class BackendStatusBanner extends React.Component {
   constructor(props) {
@@ -7,27 +11,10 @@ class BackendStatusBanner extends React.Component {
     this.state = {
       apiConnectionStatus: 200,
     };
-
-    this.apiUrl = '/assets/course-v1:edX+DemoX+Demo_Course/?page=0&page_size=50&sort=sort&asset_type=';
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      this.apiUrl = `/api${this.apiUrl}`;
-    }
   }
 
   componentDidMount() {
-    fetch(this.apiUrl, {
-      credentials: 'same-origin',
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-      .then((response) => {
-        if (response.status !== this.state.apiConnectionStatus) {
-          this.setState({
-            apiConnectionStatus: response.status,
-          });
-        }
-      });
+    this.props.pingStudio();
   }
 
   render() {
@@ -35,10 +22,23 @@ class BackendStatusBanner extends React.Component {
       null :
       (
         <div className="api-error">
-          {statusMap[this.state.apiConnectionStatus]}
+          {statusMap[this.props.apiConnectionStatus]}
         </div>
       );
   }
 }
 
-export default BackendStatusBanner;
+BackendStatusBanner.propTypes = {
+  apiConnectionStatus: PropTypes.string.isRequired,
+  pingStudio: PropTypes.func.isRequired,
+};
+
+const WrappedBackendStatusBanner = connect(
+  state => ({
+    apiConnectionStatus: state.apiConnectionStatus,
+  }), dispatch => ({
+    pingStudio: () => dispatch(pingStudio()),
+  }),
+)(BackendStatusBanner);
+
+export default WrappedBackendStatusBanner;
--- src/data/store.js
+++ src/data/store.js
@@ -1,9 +1,46 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 const ADD_ASSETS_FILTER = 'ADD_ASSETS_FILTER';
 const UPDATE_ASSETS = 'UPDATE_ASSETS';
 
-function list(state = {}, action) {
+const PING_RESPONSE = 'PING_RESPONSE';
+
+function pingResponse(response) {
+  return {
+    type: PING_RESPONSE,
+    status: response.status,
+  };
+}
+
+export function pingStudio() {
+  return dispatch =>
+    fetch('/api/assets/course-v1:edX+DemoX+Demo_Course/?page=0&page_size=50&sort=sort&asset_type=', {
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => {
+        // if (response.status !== this.state.apiConnectionStatus) {
+        //   this.setState({
+        //     apiConnectionStatus: response.status,
+        //   });
+        // }
+        dispatch(pingResponse(response));
+      });
+}
+
+function apiConnectionStatus(state = null, action) {
+  switch (action.type) {
+    case PING_RESPONSE:
+      return action.status;
+    default:
+      return state;
+  }
+}
+
+function assetsList(state = {}, action) {
   switch (action.type) {
     case UPDATE_ASSETS:
       return Object.assign({}, state, action.filter);
@@ -12,7 +49,7 @@ function list(state = {}, action) {
   }
 }
 
-function filters(state = {}, action) {
+function assetsFilters(state = {}, action) {
   switch (action.type) {
     case ADD_ASSETS_FILTER:
       return Object.assign({}, state, action.filter);
@@ -22,10 +59,14 @@ function filters(state = {}, action) {
 }
 
 const rootReducer = combineReducers({
-  list,
-  filters,
+  assetsList,
+  assetsFilters,
+  apiConnectionStatus,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware),
+);
 
 export default store;
