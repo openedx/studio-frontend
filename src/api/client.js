ab99a4ec2ced6c5667d64306f56d4fed4eed492f
Tyler Hallada <tyler@hallada.net>
2017-09-07 13:14:27 -0400

    API client (#18)
    * Implement api client function: callAssets
    
    * Rename callAssets to getAssets
    
    * Address PR comments

---
+++ src/api/client.js
@@ -0,0 +1,13 @@
+/* eslint import/prefer-default-export: "off" */
+import endpoints from './endpoints';
+
+export function getAssets(courseId, { page = 0, pageSize = 50, sort = 'sort', assetType = '' }) {
+  return fetch(
+    `${endpoints.assets}/${courseId}/?page=${page}&page_size=${pageSize}&sort=${sort}&asset_type=${assetType}`, {
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  );
+}
