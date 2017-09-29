b7d7952bde3a1d30ecf012b49e4d76ebeb55ff15
Farhanah Sheets <fsheets@edx.org>
2017-09-29 17:34:03 -0400

    Initial addition of FA trashcan to Assets Table View

--- src/AssetsPage/AssetsTable/index.jsx
+++ src/AssetsPage/AssetsTable/index.jsx
@@ -1,30 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Table from 'paragon/src/Table';
+import Button from 'paragon/src/Button';
 
-const AssetsTable = ({ assetsList }) => (
-  (!assetsList.length) ? (
-    <span>Loading....</span>
-  ) : (
-    <Table
-      columns={[
-        {
-          label: 'Name',
-          key: 'display_name',
-        },
-        {
-          label: 'Type',
-          key: 'content_type',
-        },
-        {
-          label: 'Date Added',
-          key: 'date_added',
-        },
-      ]}
-      data={assetsList}
-    />
-  )
-);
+import styles from '../styles.scss';
+
+class AssetsTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      finalAssetsList: [],
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.assetsList.length !== this.props.assetsList.length) {
+      this.addDeleteButton();
+    }
+  }
+
+  addDeleteButton() {
+    const newAssetsList = this.props.assetsList.map((asset) => {
+      const currentAsset = Object.assign({}, asset);
+      const deleteButton = (<Button className={['fa', 'fa-trash', styles['icon']]} display='' buttonType='link' />);
+      currentAsset.delete_asset = deleteButton;
+      return currentAsset;
+    });
+
+    this.setState({
+      finalAssetsList: newAssetsList,
+    });
+  }
+
+  render() {
+    return (!this.props.assetsList.length) ? (
+      <span>Loading....</span>
+    ) : (
+      <Table
+        columns={[
+          {
+            label: 'Name',
+            key: 'display_name',
+          },
+          {
+            label: 'Type',
+            key: 'content_type',
+          },
+          {
+            label: 'Date Added',
+            key: 'date_added',
+          },
+          {
+            label: 'Delete Asset',
+            key: 'delete_asset',
+          },
+        ]}
+        data={this.state.finalAssetsList}
+      />
+    );
+  }
+}
 
 AssetsTable.propTypes = {
   assetsList: PropTypes.arrayOf(PropTypes.object).isRequired,
