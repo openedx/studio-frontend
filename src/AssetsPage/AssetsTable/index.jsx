7a2a98ffa6316ac1da17abd1b53725d4662a5b98
Farhanah Sheets <fsheets@edx.org>
2017-10-18 12:15:13 -0400

    Add StatusAlert on Delete of Asset

--- src/AssetsPage/AssetsTable/index.jsx
+++ src/AssetsPage/AssetsTable/index.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import Table from '@edx/paragon/src/Table';
 import Button from '@edx/paragon/src/Button';
 import Modal from '@edx/paragon/src/Modal';
+import StatusAlert from '@edx/paragon/src/StatusAlert';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 
 import FontAwesomeStyles from 'font-awesome/css/font-awesome.min.css';
-import { deleteAsset } from '../../data/actions/assets';
-
+import { assetActions } from '../../data/constants/actionTypes';
+import { clearAssetsStatus, deleteAsset } from '../../data/actions/assets';
 
 export class AssetsTable extends React.Component {
   constructor(props) {
@@ -16,25 +17,29 @@ export class AssetsTable extends React.Component {
     this.state = {
       displayAssetsList: [],
       modalOpen: false,
+      statusAlertOpen: false,
       assetToDelete: {},
       elementToFocusOnModalClose: {},
+      statusAlertRef: {},
     };
 
     this.onDeleteClick = this.onDeleteClick.bind(this);
     this.deleteAsset = this.deleteAsset.bind(this);
-    this.addDeleteButton = this.addDeleteButton.bind(this);
+    this.addSupplementalTableElements = this.addSupplementalTableElements.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.closeStatusAlert = this.closeStatusAlert.bind(this);
+    this.renderStatusAlert = this.renderStatusAlert.bind(this);
 
     this.trashcanRefs = {};
   }
 
   componentDidMount() {
-    this.addDeleteButton();
+    this.addSupplementalTableElements();
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.assetsList.length !== this.props.assetsList.length) {
-      this.addDeleteButton();
+      this.addSupplementalTableElements();
     }
   }
 
@@ -48,8 +53,27 @@ export class AssetsTable extends React.Component {
     });
   }
 
-  addDeleteButton() {
-    const newAssetsList = this.props.assetsList.map((asset) => {
+  getNextFocusElementOnDelete(assetToDelete) {
+    const { assetsStatus } = this.props;
+    let rowToDelete = assetToDelete.row_key;
+    let focusAsset = assetToDelete;
+
+    switch (assetsStatus.type) {
+      case assetActions.DELETE_ASSET_SUCCESS:
+        if (rowToDelete > 0) {
+          rowToDelete -= 1;
+        }
+        focusAsset = this.state.displayAssetsList.find(asset => asset.row_key === (rowToDelete));
+        break;
+      default:
+        break;
+    }
+
+    return this.trashcanRefs[focusAsset.id];
+  }
+
+  addSupplementalTableElements() {
+    const newAssetsList = this.props.assetsList.map((asset, index) => {
       const currentAsset = Object.assign({}, asset);
       const deleteButton = (<Button
         className={[FontAwesomeStyles.fa, FontAwesomeStyles['fa-trash']]}
@@ -57,10 +81,14 @@ export class AssetsTable extends React.Component {
         buttonType={'light'}
         aria-label={`Delete ${currentAsset.display_name}`}
         onClick={() => { this.onDeleteClick(currentAsset.id); }}
-        inputRef={(ref) => { this.trashcanRefs[asset.id] = ref; }}
+        inputRef={(ref) => { this.trashcanRefs[currentAsset.id] = ref; }}
       />);
 
+      // TODO: create lockButton
+
       currentAsset.delete_asset = deleteButton;
+      // TODO: currentAsset.lock_asset = lockButton;
+      currentAsset.row_key = index;
       return currentAsset;
     });
 
@@ -74,15 +102,23 @@ export class AssetsTable extends React.Component {
     this.state.elementToFocusOnModalClose.focus();
   }
 
+  closeStatusAlert() {
+    this.setState({
+      statusAlertOpen: false,
+    });
+    this.getNextFocusElementOnDelete(this.state.assetToDelete).focus();
+    this.props.clearAssetsStatus();
+  }
+
   deleteAsset() {
     this.props.deleteAsset(this.props.assetsParameters, this.state.assetToDelete.id);
     this.setState({ modalOpen: false });
 
-    // TODO shift focus to banner, something like:
-    // this.setState({
-    //  elementToFocusOnModalClose: <alertStatus>
-    // })
-    // this.state.elementToFocusOnModalClose.focus();
+    this.setState({
+      elementToFocusOnModalClose: this.statusAlertRef,
+      statusAlertOpen: true,
+    });
+    this.state.elementToFocusOnModalClose.focus();
   }
 
   renderModal() {
@@ -90,7 +126,7 @@ export class AssetsTable extends React.Component {
       <Modal
         open={this.state.modalOpen}
         title={`Delete ${this.state.assetToDelete.display_name}`}
-        body={this.renderBody()}
+        body={this.renderModalBody()}
         closeText="Cancel"
         onClose={this.closeModal}
         buttons={[
@@ -104,7 +140,7 @@ export class AssetsTable extends React.Component {
     );
   }
 
-  renderBody() {
+  renderModalBody() {
     return (
       <div>
         <span className={classNames(FontAwesomeStyles.fa, FontAwesomeStyles['fa-exclamation-triangle'])} aria-hidden="true" />
@@ -115,11 +151,48 @@ export class AssetsTable extends React.Component {
     );
   }
 
+  renderStatusAlert() {
+    const { assetsStatus } = this.props;
+    const deleteAssetName = this.state.assetToDelete.display_name;
+    let alertDialog = `Deleting ${deleteAssetName}`;
+    let alertType = 'info';
+
+    switch (assetsStatus.type) {
+      case assetActions.ASSET_XHR_FAILURE:
+        alertDialog = `Unable to delete ${deleteAssetName}.`;
+        alertType = 'danger';
+        break;
+      case assetActions.DELETE_ASSET_SUCCESS:
+        alertDialog = `${deleteAssetName} has been deleted.`;
+        alertType = 'success';
+        break;
+      default:
+        break;
+    }
+
+    const statusAlert = (
+      <StatusAlert
+        alertType={alertType}
+        open={this.state.statusAlertOpen}
+        dialog={alertDialog}
+        onClose={this.closeStatusAlert}
+        ref={(input) => { this.statusAlertRef = input; }}
+      />
+    );
+
+    return (
+      <div>
+        {statusAlert}
+      </div>
+    );
+  }
+
   render() {
     return (!this.props.assetsList.length) ? (
       <span>Loading....</span>
     ) : (
       <div>
+        {this.renderStatusAlert()}
         <Table
           columns={[
             {
@@ -152,7 +225,12 @@ AssetsTable.propTypes = {
   assetsParameters: PropTypes.objectOf(
     PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool, PropTypes.object]),
   ).isRequired,
+  assetsStatus: PropTypes.shape({
+    response: PropTypes.object,
+    type: PropTypes.string,
+  }).isRequired,
   deleteAsset: PropTypes.func.isRequired,
+  clearAssetsStatus: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -163,6 +241,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   deleteAsset: (assetsParameters, assetId) => dispatch(deleteAsset(assetsParameters, assetId)),
+  clearAssetsStatus: () => dispatch(clearAssetsStatus()),
 });
 
 const WrappedAssetsTable = connect(
