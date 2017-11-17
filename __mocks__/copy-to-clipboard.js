81037ca4c9e82bed18894e4ba00cc8a322d57e28
Michael Roytman <mroytman@edx.org>
2017-11-17 15:23:26 -0500

    * Add a CopyButton component to copy text to the clipboard (#49)
    * Add CopyButton to AssetsTable page to copy studio and web URLs
    * Add tests for CopyButton
    * Add tests for CopyButton use in AssetsTable

---
+++ __mocks__/copy-to-clipboard.js
@@ -0,0 +1,1 @@
+export default jest.genMockFromModule('copy-to-clipboard');
---
+++ src/components/CopyButton/index.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@edx/paragon/src/Button';
+import copy from 'copy-to-clipboard';
+import styles from './CopyButton.scss';
+
+export default class CopyButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wasClicked: false,
+    };
+
+    this.onClick = this.onClick.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.setButtonRef = this.setButtonRef.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      wasClicked: true,
+    });
+
+    copy(`${this.props.textToCopy}`);
+
+    /*
+      The above copy command removes focus from the button, so we
+      have to explicitly set it back. The copy command works by
+      inserting a span containing the text to be copied into the
+      DOM and running document.execCommand('copy') on it. Some
+      part of this process shifts focus.
+    */
+    this.buttonRef.focus();
+    /*
+      This executes the callback passed in by the parent to
+       correctly update the screen-reader text span in the
+       AssetsTable.
+    */
+    this.props.onCopyButtonClick(true);
+  }
+
+  onBlur() {
+    this.setState({
+      wasClicked: false,
+    });
+    /*
+      This executes the callback passed in by the parent to
+       correctly update the screen-reader text span in the
+       AssetsTable.
+    */
+    this.props.onCopyButtonClick(false);
+  }
+
+  setButtonRef(input) {
+    this.buttonRef = input;
+  }
+
+  render() {
+    const label = this.state.wasClicked ? this.props.onCopyLabel : this.props.label;
+
+    return (
+      <Button
+        className={[styles['copy-button']]}
+        aria-label={this.props.ariaLabel}
+        label={label}
+        inputRef={this.setButtonRef}
+        onClick={this.onClick}
+        onBlur={this.onBlur}
+        onMouseUp={this.onMouseUp}
+        onMouseDown={this.onMouseDown}
+      />
+    );
+  }
+}
+
+CopyButton.propTypes = {
+  ariaLabel: PropTypes.string,
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+  onCopyLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
+  textToCopy: PropTypes.string.isRequired,
+  onCopyButtonClick: PropTypes.func,
+};
+
+CopyButton.defaultProps = {
+  onCopyButtonClick: () => {},
+  ariaLabel: 'Copy',
+};
