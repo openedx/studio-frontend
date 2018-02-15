59373046c7f33fbe8e4ec3727e39196fb6c959b3
Tyler Hallada <tyler@hallada.net>
2018-02-15 11:57:24 -0500

    Babel-polyfill fix (#127)
    * Import babel-polyfill if not already imported
    
    * Bump version to 1.2.5
    
    * Fix typo in comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,12 @@
-import 'babel-polyfill'; // general ES2015 polyfill (e.g. promise)
+/* eslint-disable global-require,no-underscore-dangle */
+if ((typeof window !== 'undefined' && !window._babelPolyfill) ||
+   (typeof global !== 'undefined' && !global._babelPolyfill)) {
+  // Don't load babel-polyfill if already loaded: https://github.com/babel/babel/issues/4019
+  require('babel-polyfill'); // general ES2015 polyfill (e.g. promise)
+}
+/* eslint-enable global-require no-underscore-dangle */
+
+/* eslint-disable import/first */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -8,6 +16,7 @@ import enLocaleData from 'react-intl/locale-data/en';
 import WrappedAssetsPage from './components/AssetsPage/container';
 import BackendStatusBanner from './components/BackendStatusBanner';
 import store from './data/store';
+/* eslint-enable import/first */
 
 addLocaleData(enLocaleData);
 
