a41bb0885dfd3903796153cfb453922a1ea98e00
Tyler Hallada <thallada@edx.org>
2018-02-15 11:33:14 -0500

    Fix typo in comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable global-require,no-underscore-dangle */
 if ((typeof window !== 'undefined' && !window._babelPolyfill) ||
    (typeof global !== 'undefined' && !global._babelPolyfill)) {
-  // Don'disablet load babel-polyfill if already loaded: https://github.com/babel/babel/issues/4019
+  // Don't load babel-polyfill if already loaded: https://github.com/babel/babel/issues/4019
   require('babel-polyfill'); // general ES2015 polyfill (e.g. promise)
 }
 /* eslint-enable global-require no-underscore-dangle */
