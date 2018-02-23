c4f09c8707f53c272bfa4d9fc0a7a8a352d22e14
Eric Fischer <efischer@edx.org>
2018-02-23 16:46:17 -0500

    asdasdasds

--- src/data/i18n/locales/lang_scripts/fr.jsx
+++ src/data/i18n/locales/lang_scripts/fr.jsx
@@ -3,13 +3,4 @@ import { addLocaleData } from 'react-intl';
 
 import messages from '../json/fr.json'
 
-export default class SFELanguage {
-  render() {
-    addLocaleData(frLocaleData);
-    return (
-      <script type="application/json" id="SFE_i18n_messages">
-        { messages }
-      </script>
-    );
-  }
-}
+addLocaleData(frLocaleData);
