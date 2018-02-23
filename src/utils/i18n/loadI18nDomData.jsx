1a2640176d0c3fe87f532764a304bd68318af747
Eric Fischer <efischer@edx.org>
2018-02-23 16:59:42 -0500

    moar

--- src/utils/i18n/loadI18nDomData.jsx
+++ src/utils/i18n/loadI18nDomData.jsx
@@ -1,25 +1,12 @@
-import enLocaleData from 'react-intl/locale-data/en';
-import { addLocaleData } from 'react-intl';
-
-const loadI18nDomData = () => {
-  const retDict = {};
-  let localeData;
-
+export default loadI18nDomData = () => {
   try {
-    const domDefinedLocaleData = JSON.parse(document.getElementById('SFE_i18n_localeScript').innerHTML);
-    localeData = domDefinedLocaleData.data;
-    retDict.locale = domDefinedLocaleData.locale;
-    retDict.messages = JSON.parse(document.getElementById('SFE_i18n_messages').innerHTML);
+    const domLocale = document.getElementById('SFE_i18n_locale').innerHTML;
+    const domMessages = JSON.parse(document.getElementById('SFE_i18n_messages').innerHTML);
   } catch (err) {
     // fail back to default english values if any errors
-    localeData = enLocaleData;
-    retDict.locale = 'en';
-    // an empty dict will yield defaultMessages on fallback, no problem there
-    retDict.messages = {};
+    return { "locale": "en", "messages": {} };
   }
-  addLocaleData(localeData);
-
-  return retDict;
+  return {"locale": domLocale, "messages": domMessages};
 };
 
 export default loadI18nDomData;
