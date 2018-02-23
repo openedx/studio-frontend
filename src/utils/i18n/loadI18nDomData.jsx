9669a40a7a10adc58e7a4c7b341af2730930f310
Eric Fischer <efischer@edx.org>
2018-02-23 17:43:43 -0500

    asdasdasdasd

--- src/utils/i18n/loadI18nDomData.jsx
+++ src/utils/i18n/loadI18nDomData.jsx
@@ -2,11 +2,12 @@ const loadI18nDomData = () => {
   try {
     const domLocale = document.getElementById('SFE_i18n_locale').innerHTML;
     const domMessages = JSON.parse(document.getElementById('SFE_i18n_messages').innerHTML);
+    return {"locale": domLocale, "messages": domMessages};
   } catch (err) {
-    // fail back to default english values if any errors
-    return { "locale": "en", "messages": {} };
+    console.log(err);
   }
-  return {"locale": domLocale, "messages": domMessages};
+  // fail back to default english values if any errors
+  return { "locale": "en", "messages": {} };
 };
 
 export default loadI18nDomData;
