eef3dd0d52baa17baebc7185de3ac48d027f4f0f
Eric Fischer <efischer@edx.org>
2018-02-26 09:46:41 -0500

    locale data scripts are no longer passed in DOM

--- src/utils/i18n/loadI18nDomData.jsx
+++ src/utils/i18n/loadI18nDomData.jsx
@@ -1,25 +1,22 @@
 import enLocaleData from 'react-intl/locale-data/en';
 import { addLocaleData } from 'react-intl';
 
-const loadI18nDomData = () => {
-  const retDict = {};
-  let localeData;
+import localeDataMap from '../../data/i18n/locales/json/currentlySupportedLangs';
 
+const loadI18nDomData = () => {
   try {
-    const domDefinedLocaleData = JSON.parse(document.getElementById('SFE_i18n_localeScript').innerHTML);
-    localeData = domDefinedLocaleData.data;
-    retDict.locale = domDefinedLocaleData.locale;
-    retDict.messages = JSON.parse(document.getElementById('SFE_i18n_messages').innerHTML);
+    const domDefinedData = JSON.parse(document.getElementById('SFE_i18n_data').innerHTML);
+    const localeData = localeDataMap[domDefinedData.locale];
+    addLocaleData(localeData);
+    return domDefinedData;
   } catch (err) {
     // fail back to default english values if any errors
-    localeData = enLocaleData;
-    retDict.locale = 'en';
-    // an empty dict will yield defaultMessages on fallback, no problem there
-    retDict.messages = {};
+    addLocaleData(enLocaleData);
+    return {
+      locale: 'en',
+      messages: {},
+    };
   }
-  addLocaleData(localeData);
-
-  return retDict;
 };
 
 export default loadI18nDomData;
