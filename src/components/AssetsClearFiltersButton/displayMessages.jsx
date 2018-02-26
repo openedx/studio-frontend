bb3bf6c8dbbe63b2c8a13a0b9c4a28294d4cd5d8
Tyler Hallada <thallada@edx.org>
2018-02-26 12:18:28 -0500

    Change clear filters button display text

--- src/components/AssetsClearFiltersButton/displayMessages.jsx
+++ src/components/AssetsClearFiltersButton/displayMessages.jsx
@@ -3,7 +3,7 @@ import { defineMessages } from 'react-intl';
 const messages = defineMessages({
   assetsClearFiltersButtonLabel: {
     id: 'assetsClearFiltersButtonLabel',
-    defaultMessage: 'View all files',
+    defaultMessage: 'Clear all filters',
     description: 'Label for a button that clears all filters applied to a table.',
   },
 });
