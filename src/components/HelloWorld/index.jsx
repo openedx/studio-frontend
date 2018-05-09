04f9776905cf1f3cd72241da037c0a8c66a90cc4
Tyler Hallada <thallada@edx.org>
2018-05-09 15:17:29 -0400

    fix(helloworld): button opens a modal

--- src/components/HelloWorld/index.jsx
+++ src/components/HelloWorld/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from '@edx/paragon';
+import { Button, Modal } from '@edx/paragon';
+import 'font-awesome/css/font-awesome.min.css';
 
 // Sass styles are imported in JS so that we can programatically apply styles to React elements by
 // class name.  Webpack's css-loader can later rename the actual class name to a name that is scoped
@@ -12,16 +13,36 @@ export default class HelloWorld extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      modalOpen: false,
+    };
+
     this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
   }
 
   handleButtonClick() {
-    alert('Hello!');
+    this.setState({
+      modalOpen: true,
+    });
+  }
+
+  handleModalClose() {
+    this.setState({
+      modalOpen: false,
+    });
   }
 
   render() {
     return (
       <div className={styles['hello-world']}>
+        <Modal
+          open={this.state.modalOpen}
+          title="Hello Modal"
+          body="Hello, world!"
+          onClose={this.handleModalClose}
+          parentSelector={`.${styles['hello-world']}`}
+        />
         <Button
           buttonType="primary"
           label="Hello, world!"
