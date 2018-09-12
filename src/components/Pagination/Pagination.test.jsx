9994f619a6b6fca5d34167615541b12c08c858db
Matthew Hughes <mhughes@C02TP0LBG8WL.tld>
2018-09-12 15:08:17 -0400

    Removed pagination tests which test stuff now abstracted by paragon
    and added one test to check some additional coverage area

--- src/components/Pagination/Pagination.test.jsx
+++ src/components/Pagination/Pagination.test.jsx
@@ -21,9 +21,7 @@ const totalPages = Math.ceil(
 
 describe('<Pagination />', () => {
   let wrapper,
-      paragonPagination,
-      // TODO: these should probably be eliminated since they cross component boundaries
-      pageLinks;
+      paragonPagination;
 
   beforeEach(() => {
     wrapper = mountWithIntl(
@@ -31,8 +29,6 @@ describe('<Pagination />', () => {
         {...defaultProps}
       />,
     );
-
-    pageLinks = wrapper.find('.page-item .page-link');
     paragonPagination = wrapper.find(ParagonPagination);
   });
 
@@ -61,73 +57,14 @@ describe('<Pagination />', () => {
 
     expect(currentPage).toEqual(1);
     expect(paragonPagination.prop('currentPage')).toEqual(currentPage + 1);
-
   });
-  describe('break link', () => {
-    let breakLink;
-
-    beforeEach(() => {
-      breakLink = wrapper.find('.disabled.page-link');
+  it('calls provided updatePage function when paragon\'s pagination changes page number', () => {
+    const updatePageSpy = jest.fn();
+    wrapper.setProps({
+      updatePage: updatePageSpy
     });
-
-    it('has disabled break link', () => {
-      expect(breakLink).toHaveLength(1);
-      expect(breakLink.text()).toEqual('...button is disabled');
-    });
-    it('has sr-only text on disabled break link', () => {
-      const breakScreenReader = breakLink.find('.sr-only');
-      expect(breakScreenReader).toHaveLength(1);
-      expect(breakScreenReader.text()).toEqual('button is disabled');
-    });
-  });
-  describe('previous link', () => {
-    let previousLink;
-
-    beforeEach(() => {
-      wrapper.setProps({
-        ...defaultProps,
-        assetsListMetadata: {
-          ...defaultProps.assetsListMetadata,
-          page: 0,
-        },
-      });
-
-      previousLink = wrapper.find('.disabled > .page-link');
-    });
-    it('is disabled when page is first page', () => {
-      expect(previousLink).toHaveLength(1);
-      expect(previousLink.text()).toEqual('previousbutton is disabled');
-    });
-    it('has sr-only text', () => {
-      const breakScreenReader = previousLink.find('.sr-only');
-      expect(breakScreenReader).toHaveLength(1);
-      expect(breakScreenReader.text()).toEqual('button is disabled');
-    });
-  });
-  describe('next link has sr-only text when disabled', () => {
-    let nextLink;
-
-    beforeEach(() => {
-      wrapper.setProps({
-        ...defaultProps,
-        assetsListMetadata: {
-          ...defaultProps.assetsListMetadata,
-          page: totalPages - 1,
-        },
-      });
-
-      pageLinks.last().simulate('click');
-
-      nextLink = wrapper.find('.disabled > .page-link');
-    });
-    it('is disabled when page is last page', () => {
-      expect(nextLink).toHaveLength(1);
-      expect(nextLink.text()).toEqual('nextbutton is disabled');
-    });
-    it('has sr-only text when disabled', () => {
-      const breakScreenReader = nextLink.find('.sr-only');
-      expect(breakScreenReader).toHaveLength(1);
-      expect(breakScreenReader.text()).toEqual('button is disabled');
-    });
+    paragonPagination.prop('onPageSelect')(7);
+    expect(updatePageSpy).toHaveBeenCalledTimes(1);
+    expect(updatePageSpy).toHaveBeenCalledWith(6, defaultProps.courseDetails);
   });
 });
