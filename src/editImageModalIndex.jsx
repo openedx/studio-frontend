3080795f9b09cbff6bbcae917d0078aa9cf2e2da
Ghassan Maslamani <ghassan.maslamani@gmail.com>
2022-04-29 01:04:58 +0300

    fix: empty build due babel incorrect configuration
      The PR #315 added @babel/core >= 7.*. upgrading/using babel v7
      requires, making certian changes, e.g using @babel...etc. This
      lead to `npm run build` being interrupted/exited.without
      generating the /dist folder.
    
      Luckily, there is a pacakge to automate migration of config
      using `npx babel-upgrade --write`, followed by `npm install`, and
      few minor changes, the dist folder came to life again.
    
      This shall fixes this openedx/build-test-release-wg#166. and
      probably making this openedx/edx-platform#30309 unnecessary.
    
      Note: This change adds "^7.0.0-bridge.0" as devDep, this is
      needed because we have pacakges that uses Babel 6, which are
      jest and babel-jest. once we upgrade to babel-jest and jest
      >=24.*.* the pacakge shall be removed.

--- src/editImageModalIndex.jsx
+++ src/editImageModalIndex.jsx
@@ -2,7 +2,7 @@
 if ((typeof window !== 'undefined' && !window._babelPolyfill) ||
   (typeof global !== 'undefined' && !global._babelPolyfill)) {
   // Don't load babel-polyfill if already loaded: https://github.com/babel/babel/issues/4019
-  require('babel-polyfill'); // general ES2015 polyfill (e.g. promise)
+  require('@babel/polyfill'); // general ES2015 polyfill (e.g. promise)
 }
 /* eslint-enable global-require no-underscore-dangle */
 
